ab248dec9d1ecaf32cb371065ca18f9c111654a9
Christoffer <christoffer.bjelke@outlook.com>
2020-02-20 11:59:24 +0100

    uke6

---
+++ uke6/Boks.java
@@ -0,0 +1,11 @@
+class Boks<T> { // E, K, V, N
+    private T data;
+
+    public Boks(T data) {
+        this.data = data;
+    }
+
+    public T hentData() {
+        return data;
+    }
+}
---
+++ uke6/Main.java
@@ -0,0 +1,24 @@
+class Main {
+    public static void main(String[] args) {
+        Node start = new Node("hei");
+        Node nyNode = new Node("på");
+        start.settNeste(nyNode);
+        nyNode.settNeste(new Node("deg"));
+
+        // System.out.println(start.hentNeste().hentNeste().hentData());
+
+        Node node = start;
+        while (node != null) {
+            System.out.println(node.hentData());
+            node = node.hentNeste();
+        }
+
+        System.out.println();
+
+        Boks<String> nyBoks = new Boks<String>("Hei");
+        Boks<Integer> intBoks = new Boks<Integer>(1);
+
+        System.out.println(nyBoks.hentData());
+        System.out.println(intBoks.hentData());
+    }
+}
